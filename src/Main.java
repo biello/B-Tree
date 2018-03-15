d41ad3b97e4c9b7412035c9e92ed886615086811
Biello <653091528@qq.com>
2018-03-16 00:57:30 +0800

    为方便观察结果，增加控制台打印B树的方法

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,31 @@ public class Main {
     public static void main(String[] args) {
 
         BTreeNode node = new BTreeNode(3);
-        node.insert(35);
-        node.insert(17);
-        node.insert(18);
-
-        while(node.parent != null) {
-            node = node.parent;
-        }
-        System.out.println(node.values);
+        node = BTreeNode.insert(node, 35);
+        node = BTreeNode.insert(node, 17);
+        node = BTreeNode.insert(node, 18);
+        node = BTreeNode.insert(node,19);
+        printBTree(node, 0);
     }
 
-    public static List<BTreeNode> getEmptyNodes(int num) {
+    public static void printBTree(BTreeNode node, int depth) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < depth; i++) {
+            sb.append("|   ");
+        }
+        if(depth > 0) {
+            sb.append("|---");
+        }
+        System.out.println(sb.toString() + node.values);
+        for(BTreeNode child : node.children) {
+            printBTree(child, depth+1);
+        }
+    }
+
+    public static List<BTreeNode> getEmptyNodes(int num, int m) {
         List<BTreeNode> emptyNodes = new LinkedList<>();
         while(num-- != 0) {
-            emptyNodes.add(new BTreeNode());
+            emptyNodes.add(new BTreeNode(m));
         }
         return  emptyNodes;
     }
