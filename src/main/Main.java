88db6f64479b6a9e28db29f1df816a070b41d19c
Biello <653091528@qq.com>
2018-03-25 15:22:05 +0800

    增加删除关键字的测试用例

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ import ind.biello.node.BTreeNode;
 public class Main {
 
     public static void main(String[] args) {
-       testDeleteCase4();
+       testDeleteCase5();
     }
 
 
@@ -74,7 +74,31 @@ public class Main {
         node = node.delete(35);
         node.print();
         System.out.println("-----------------");
+    }
 
+    public static void testDeleteCase5() {
+        BTreeNode node = new BTreeNode(3);
+        node = node.insert(3);
+        node = node.insert(24);
+        node = node.insert(37);
+        node = node.insert(45);
+        node = node.insert(50);
+        node = node.insert(53);
+        node = node.insert(61);
+        node = node.insert(90);
+        node = node.insert(100);
+        node = node.insert(70);
+        System.out.println("--------原树：---------");
+        node.print();
+        System.out.println("--------删除50后：---------");
+        node = node.delete(50);
+        node.print();
+        System.out.println("--------删除53后：---------");
+        node = node.delete(53);
+        node.print();
+        System.out.println("--------删除37后：---------");
+        node = node.delete(37);
+        node.print();
     }
 
 }
